05475e8ac2363c8f275939742f05884c58d2c69c
Your Name <you@example.com>
2025-06-10 09:59:07 -0400

    module 3-2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,9 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://tnorappelz-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://tnorappelz-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
-CSRF_TRUSTED_ORIGINS = ['https://tnorappelz-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://tnorappelz-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
